a17357c23dbb2eb24553da625b8b0d6085abe322
drive <driveasano@gmail.com>
2026-01-13 17:46:23 +0900

    convex and clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import {
-    ClerkProvider,
-    OrganizationSwitcher,
-    SignInButton,
-    SignUpButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { jaJP } from "@clerk/localizations";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
+import ConvexClientProvider from "./ConvexClientProvider";
+import Header from "@/components/header";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -43,21 +37,10 @@ export default function RootLayout({
                         "antialiased"
                     )}
                 >
-                    <header className="flex justify-end items-center p-4 gap-4 h-16">
-                        <OrganizationSwitcher />
-                        <SignedOut>
-                            <SignInButton />
-                            <SignUpButton>
-                                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                                    Sign Up
-                                </button>
-                            </SignUpButton>
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
-                    </header>
-                    {children}
+                    <ConvexClientProvider>
+                        <Header />
+                        {children}
+                    </ConvexClientProvider>
                 </body>
             </html>
         </ClerkProvider>
