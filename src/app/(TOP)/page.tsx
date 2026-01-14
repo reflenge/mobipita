b4305fe6e1b5483042ca95c80dc1a8d7ad439601
drive <driveasano@gmail.com>
2026-01-14 20:29:07 +0900

    新しいページコンポーネントを作成し、スタイルを追加

---
+++ src/app/(TOP)/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div
+            style={{
+                // テスト用の長い背景
+                minHeight: "200vh",
+                background: "linear-gradient(to bottom, red, blue)",
+            }}
+        />
+    );
+}
