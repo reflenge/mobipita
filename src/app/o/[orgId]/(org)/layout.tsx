8c978e3f347ade3263a20e6f114100180c50ea7d
kimura <ryuusaki0714@gmail.com>
2026-02-23 00:20:31 +0900

    履歴からlayput.tsxを修復

--- src/app/o/[orgId]/(org)/layout.tsx
+++ src/app/o/[orgId]/(org)/layout.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./_components/sidebar";
 import { Link } from "@/components/link";
+
 import AutoBreadcrumb from "./_components/breadcrumb";
 import { cn } from "@/lib/utils";
 
